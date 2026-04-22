5ab8b56c57cce266163eeac75f5f6f3692edac3d
Y-Cyan-Zhang <yzhan687@jh.edu>
2026-04-22 16:51:54 -0400

    now averaging benchmark times over 20 runs

--- push_back_vs_indexing.cpp
+++ push_back_vs_indexing.cpp
@@ -22,25 +22,33 @@ double measure(F func) {
 
 int main() {
   const int N = 10000000; // 10,000,000
+  const int runs = 20;
   
-  double direct_indexing = measure([&](){
-    std::vector<int> v(N);
-    for (int i = 0; i < N; i++)
-      v[i] = 7;
-    return v[N/2];  // Force compiler to keep the work
-  });
+  double cum_direct_indexing = 0;
+  double cum_push_back = 0;
+  for (int run = 0; run < runs; run++) {
+    double direct_indexing = measure([&](){
+      std::vector<int> v(N);
+      for (int i = 0; i < N; i++)
+        v[i] = 7;
+      return v[N/2];  // Force compiler to keep the work
+    });
 
-  double pushing_back = measure([](){
-    std::vector<int> v;
-    v.reserve(N);
-    for (int i = 0; i < N; i++)
-      v.push_back(7);
-    return v[N/2];  // Force compiler to keep the work
-  });
+    double pushing_back = measure([](){
+      std::vector<int> v;
+      v.reserve(N);
+      for (int i = 0; i < N; i++)
+        v.push_back(7);
+      return v[N/2];  // Force compiler to keep the work
+    });
 
-  std::cout << "Testing direct indexing vs push_back for 10,000,000 vector writes, both initialized as to not have to resize the vector\n";
-  std::cout << "Direct indexing: " << direct_indexing << " ms\n";
-  std::cout << "Push back: " << pushing_back << " ms\n";
+    cum_direct_indexing += direct_indexing;
+    cum_push_back += pushing_back;
+  }
+
+  std::cout << "Testing direct indexing vs push_back for 10,000,000 vector writes averaged of 20 runs. (Both initialized as to not have to resize the vector)\n";
+  std::cout << "Direct indexing: " << cum_direct_indexing / runs << " ms\n";
+  std::cout << "Push back: " << cum_push_back / runs << " ms\n";
 
   return 0;
 }
